5fdc3a1ba59f3de216ee7cb6bfeddde8537a3477
Jen Nest <154011445+jenneft@users.noreply.github.com>
2024-03-21 13:51:44 -0700

    Week 1 created

---
+++ src/JenProject/week_01/divisionWithoutOperator.java
@@ -0,0 +1,11 @@
+package JenProject.week_01;
+
+public class divisionWithoutOperator {
+    public static void main(String[] args) {
+        //Write a method that can divide two numbers without using division operator
+
+        String str="Emma";
+
+    }
+
+}
---
+++ src/JenProject/week_01/oddOrEven.java
@@ -0,0 +1,8 @@
+package JenProject.week_01;
+
+public class oddOrEven {
+    public static void main(String[] args) {
+        //Write  a method which can identifies given number is even or odd
+    int i=1;
+    }
+}
