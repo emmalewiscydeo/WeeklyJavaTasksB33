07dfad7bea8b03788f9df5c8ccc765b6fe26a839
emmalewiscydeo <emma@cydeo.com>
2024-03-28 20:01:15 +0300

    oddOrEven solved and creating conflict

--- src/JenProject/week_01/divisionWithoutOperator.java
+++ src/JenProject/week_01/divisionWithoutOperator.java
@@ -4,7 +4,7 @@ public class divisionWithoutOperator {
     public static void main(String[] args) {
         //Write a method that can divide two numbers without using division operator
 
-        String str="Emma";
+        String str="Conflict";
 
     }
 
